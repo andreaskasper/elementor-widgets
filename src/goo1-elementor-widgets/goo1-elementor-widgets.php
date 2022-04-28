21f6d71c56d187915084b3d2c4fed1d859699467
Andreas Kasper <andreas.kasper@goo1.de>
2022-04-28 19:39:44 +0200

    Bug Danceevent override repariert
    Nun sollte das Plugin wieder gehen :-)

--- src/goo1-elementor-widgets/goo1-elementor-widgets.php
+++ src/goo1-elementor-widgets/goo1-elementor-widgets.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/andreaskasper/
  * Description: Implement important functions and shortcodes for your danceevent
  * Author: Andreas Kasper
- * Version: 0.1.108
+ * Version: 0.1.109
  * Author URI: https://github.com/andreaskasper/
  * Network: True
  * Text Domain: goo1-elementor
