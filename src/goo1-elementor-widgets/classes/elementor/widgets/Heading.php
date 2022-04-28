abd3c0b31cddd6b3758dd565fd92f1dd2fe22ddd
Andreas Kasper <andreas.kasper@goo1.de>
2022-04-28 23:25:59 +0200

    Heading: outer tag and link

--- src/goo1-elementor-widgets/classes/elementor/widgets/Heading.php
+++ src/goo1-elementor-widgets/classes/elementor/widgets/Heading.php
@@ -109,6 +109,54 @@ class Heading extends \Elementor\Widget_Base {
 				'type' => \Elementor\Controls_Manager::TEXT
 			]
 		);
+
+		$this->add_control(
+			'space_between',
+			[
+				'label' => __( 'space between party', 'plugin-domain' ),
+				'type' => \Elementor\Controls_Manager::SWITCHER,
+				'label_on' => __( 'Yes', 'your-plugin' ),
+				'label_off' => __( 'No', 'your-plugin' ),
+				'return_value' => "yes",
+				'default' => 'yes',
+			]
+		);
+
+		$this->add_control(
+			'url_href',
+			[
+				'label' => esc_html__( 'Link', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'placeholder' => esc_html__( '', 'plugin-name' ),
+				'default' => [
+					'url' => '',
+					'is_external' => true,
+					'nofollow' => true,
+					'custom_attributes' => '',
+				],
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'html_tag',
+			[
+				'label' => esc_html__( 'HTML Tag', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'default' => 'div',
+				'options' => [
+					'h1' => esc_html__( 'H1', 'plugin-name' ),
+					'h2' => esc_html__( 'H2', 'plugin-name' ),
+					'h3' => esc_html__( 'H3', 'plugin-name' ),
+					'h4' => esc_html__( 'H4', 'plugin-name' ),
+					'h5' => esc_html__( 'H5', 'plugin-name' ),
+					'h6' => esc_html__( 'H6', 'plugin-name' ),
+					'div' => esc_html__( 'div', 'plugin-name' ),
+					'span' => esc_html__( 'span', 'plugin-name' ),
+					'p' => esc_html__( 'p', 'plugin-name' )
+				],
+			]
+		);
 		
 		/*$this->add_control(
 			'MOBILE_APP_PROMO',
@@ -176,9 +224,17 @@ class Heading extends \Elementor\Widget_Base {
     protected function render() {
 		$settings = $this->get_settings_for_display();
 
+		if (!empty($settings["url_href"])) echo('<a href="'.$settings["url_href"].'">');
+		echo('<'.$settings["html_tag"].'>');
+
 		if (!empty($settings["title1"])) echo('<span>'.$settings["title1"].'</span>');
+		if ($settings["space_between"] == "yes") echo(' ');
 		if (!empty($settings["title2"])) echo('<span>'.$settings["title2"].'</span>');
+		if ($settings["space_between"] == "yes") echo(' ');
 		if (!empty($settings["title3"])) echo('<span>'.$settings["title3"].'</span>');
+		echo('</'.$settings["html_tag"].'>');
+
+		if (!empty($settings["url_href"])) echo('</a>');
 		
 	}
 }
