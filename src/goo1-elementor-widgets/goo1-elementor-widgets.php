2129c51a3517f53f7a07d92ae9b9ac14ecdbb777
Andreas Kasper <djassi@web.de>
2022-04-28 19:33:35 +0200

    Update goo1-elementor-widgets.php

--- src/goo1-elementor-widgets/goo1-elementor-widgets.php
+++ src/goo1-elementor-widgets/goo1-elementor-widgets.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Plugin Name: Danceevent Pro
+ * Plugin Name: Elementor Essential Widgets
  * Plugin URI: https://github.com/andreaskasper/
  * Description: Implement important functions and shortcodes for your danceevent
  * Author: Andreas Kasper
- * Version: 0.1.107
+ * Version: 0.1.108
  * Author URI: https://github.com/andreaskasper/
  * Network: True
- * Text Domain: danceevent
+ * Text Domain: goo1-elementor
  */
 
 spl_autoload_register(function ($class_name) {
-	if (substr($class_name,0,24) != "plugins\\goo1\\danceevent\\") return false;
+	if (substr($class_name,0,30) != "plugins\\goo1\\elementorwidgets\\") return false;
 	$files = array(
-		__DIR__."/classes/".str_replace("\\", DIRECTORY_SEPARATOR,substr($class_name, 24)).".php"
+		__DIR__."/classes/".str_replace("\\", DIRECTORY_SEPARATOR,substr($class_name, 30)).".php"
 	);
 	foreach ($files as $file) {
 		if (file_exists($file)) {
@@ -21,7 +21,7 @@ spl_autoload_register(function ($class_name) {
 			return true;
 		}
 	}
-	die(__DIR__."/classes/".str_replace("\\", DIRECTORY_SEPARATOR,substr($class_name, 24)).".php");
+	die(__DIR__."/classes/".str_replace("\\", DIRECTORY_SEPARATOR,substr($class_name, 30)).".php");
 	return false;
 });
 
@@ -34,8 +34,8 @@ if (!class_exists("Puc_v4_Factory")) {
 	require_once(__DIR__."/plugin-update-checker/plugin-update-checker.php");
 }
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    "https://raw.githubusercontent.com/andreaskasper/wordpress-danceevent/main/dist/updater.json",
+    "https://raw.githubusercontent.com/andreaskasper/elementor-widgets/main/dist/updater.json",
     __FILE__, //Full path to the main plugin file or functions.php.
-    "goo1-danceevent"
+    "goo1-elementor-widgets"
 );
 
